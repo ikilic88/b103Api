5a85a73c0d81d44fd22820ac6d461a52a6e8a26b
ibrahim kılıç <ikilic9188@gmail.com>
2023-03-17 23:23:47 +0300

    17.03.2023

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -5,23 +5,20 @@ import java.util.Map;
 
 public class JsonPlaceHolderTestData {
 
-    public Map<String,Object> expectedDataMethod(Integer userId,String title, Boolean completed){
+    public Map<String, Object> expectedDataMethod(Integer userId, String title, Boolean completed) {
 
-        Map<String,Object> expectedData = new HashMap<>();
-        expectedData.put("userId",userId);
-        expectedData.put("title",title);
-        expectedData.put("completed",false);
+        Map<String, Object> expectedData = new HashMap<>();
+        if (userId != null) {//Eklemek istemediğim değerleri null atıyorum
+            expectedData.put("userId", userId);
+        }
+        if (title != null) {
+            expectedData.put("title", title);
+        }
 
+        if (completed != null) {
+            expectedData.put("completed", completed);
+        }
 
         return expectedData;
-
     }
-
 }
-    /*
-     Map<String,Object> expectedData = new HashMap<>();
-        expectedData.put("userId",21.0);
-        expectedData.put("title","Wash the dishes");
-        expectedData.put("completed",false);
-
-     */
