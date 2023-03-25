a7840d38e4118e4e22d0dcae1eec61cdf5f76168
ibrahim kılıç <ikilic9188@gmail.com>
2023-03-25 23:37:42 +0300

    25.03.2023

--- src/test/java/base_urls/GmiBankBaseUrl.java
+++ src/test/java/base_urls/GmiBankBaseUrl.java
@@ -4,12 +4,12 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-public class GoRestBaseUrl {
+public class GmiBankBaseUrl {
 
     protected RequestSpecification spec;
 
     @Before//Her test methodundan önce çalışır.
     public void setUp() {
-        spec = new RequestSpecBuilder().setBaseUri("https://gorest.co.in/public/v1").build();
+        spec = new RequestSpecBuilder().setBaseUri("https://www.gmibank.com").build();
     }
 }
--- src/test/java/gmibank/GetCustomer.java
+++ src/test/java/gmibank/GetCustomer.java
@@ -1,10 +1,21 @@
 package gmibank;
 
-public class Getcustomer {
+import base_urls.GmiBankBaseUrl;
+import gmibank.pojos.Account;
+import gmibank.pojos.Country;
+import gmibank.pojos.Customer;
+import gmibank.pojos.User;
+import io.restassured.response.Response;
+import org.junit.Test;
 
-          /*
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+
+public class GetCustomer extends GmiBankBaseUrl {
+       /*
     Given
-        https://www.gmibank.com/api/tp-customers/61417
+        https://www.gmibank.com/api/tp-customers/110472
     When
         User sends Get request
     Then
@@ -69,5 +80,28 @@ public class Getcustomer {
 }
      */
 
+    @Test
+    public void gmiBankCustomer() {
+//        i)   Set the URL
+        spec.pathParams("first", "api", "second", "tp-customers", "third", 110472);
 
+//        ii)  Set the expected data
+        Country country = new Country(24105, "San Jose", null);
+        User user = new User(111206, "delilah.metz", "Melva", "Bernhard", "chas.kuhlman@yahoo.com", true, "en", null, null);
+        Account account1 = new Account(2327, "omermusteri01 hesap1", 1020600, "SAVING", "ACTIVE", "2020-11-06T23:00:00Z", "2024-11-07T23:00:00Z", null, null);
+        Account account2 = new Account(107250, "New Account_6thGenQA_01", 11190, "CHECKING", "ACTIVE", "2021-11-24T23:00:00Z", "2022-11-24T23:00:00Z", null, null);
+        ArrayList<Account> accountList = new ArrayList<>();
+        accountList.add(account1);
+        accountList.add(account2);
+
+        Customer expectedData = new Customer(110472, "Melva", "Bernhard", "A", "chas.kuhlman@yahoo.com", "192-580-3408", "192-580-3408", "40207", "Apt. 634 579 Eliseo Rapids, Deanaside, AZ 53872", "New Jordanhaven", "523-50-1191", "2021-11-30T21:00:00Z", false, country, "", user, accountList);
+        System.out.println("expectedData = " + expectedData);
+//        iii) Send the request and get the response
+        Response response = given(spec).headers("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRjaF95dXp1YyIsImF1dGgiOiJST0xFX0FETUlOIiwiZXhwIjoxNjc5ODU3NjI5fQ.rbaWee8JHsgDW3fxFJNlw-YWXiaMZ6ctjeb4a1Xih0NootWK9k_W7LHgo3kicTrQ5wzNXZTzaMt6wbXamVliWA").get("{first}/{second}/{third}");
+        response.prettyPrint();
+
+//        iv)  Do assertion
+
+
+    }
 }
