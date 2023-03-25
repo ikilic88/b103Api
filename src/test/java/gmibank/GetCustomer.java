2db5e223cc0c42a897fa2dd3a0c43ff769df0b50
ibrahim kılıç <ikilic9188@gmail.com>
2023-03-25 23:35:53 +0300

    25.03.2023

---
+++ src/test/java/gmibank/GetCustomer.java
@@ -0,0 +1,73 @@
+package gmibank;
+
+public class Getcustomer {
+
+          /*
+    Given
+        https://www.gmibank.com/api/tp-customers/61417
+    When
+        User sends Get request
+    Then
+        Status code should be 200
+     And
+        Response body should be like:
+    {
+    "id": 110472,
+    "firstName": "Melva",
+    "lastName": "Bernhard",
+    "middleInitial": "A",
+    "email": "chas.kuhlman@yahoo.com",
+    "mobilePhoneNumber": "192-580-3408",
+    "phoneNumber": "192-580-3408",
+    "zipCode": "40207",
+    "address": "Apt. 634 579 Eliseo Rapids, Deanaside, AZ 53872",
+    "city": "New Jordanhaven",
+    "ssn": "523-50-1191",
+    "createDate": "2021-11-30T21:00:00Z",
+    "zelleEnrolled": false,
+    "country": {
+        "id": 24105,
+        "name": "San Jose",
+        "states": null
+    },
+    "state": "",
+    "user": {
+        "id": 111206,
+        "login": "delilah.metz",
+        "firstName": "Melva",
+        "lastName": "Bernhard",
+        "email": "chas.kuhlman@yahoo.com",
+        "activated": true,
+        "langKey": "en",
+        "imageUrl": null,
+        "resetDate": null
+    },
+    "accounts": [
+        {
+            "id": 2327,
+            "description": "omermusteri01 hesap1",
+            "balance": 1020600,
+            "accountType": "SAVING",
+            "accountStatusType": "ACTIVE",
+            "createDate": "2020-11-06T23:00:00Z",
+            "closedDate": "2024-11-07T23:00:00Z",
+            "employee": null,
+            "accountlogs": null
+        },
+        {
+            "id": 107250,
+            "description": "New Account_6thGenQA_01",
+            "balance": 11190,
+            "accountType": "CHECKING",
+            "accountStatusType": "ACTIVE",
+            "createDate": "2021-11-24T23:00:00Z",
+            "closedDate": "2022-11-24T23:00:00Z",
+            "employee": null,
+            "accountlogs": null
+        }
+    ]
+}
+     */
+
+
+}
